f56f70ffda33e56d2ad9ef1cbee7f8e8088e0983
Danis Sharipov <danshadance@mail.ru>
2022-12-27 12:08:55 +0600

    task upd user input

--- task/task.cs
+++ task/task.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.WriteLine();
+Console.WriteLine("Данная программа создает одномерный массив из строк.");
+Console.WriteLine();
+Console.Write("Задайте, пожалуйста, размер массива: ");
+int arrSize = Convert.ToInt32(Console.ReadLine());
